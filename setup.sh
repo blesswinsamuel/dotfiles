83b52e12adc8e43515b46bb2e55a06e74a0433cc
Blesswin Samuel <blesswinsamuel@gmail.com>
2018-06-09 01:59:48 +0530

    fmt

--- setup.sh
+++ setup.sh
@@ -12,48 +12,48 @@ error() {
 }
 
 main() {
-	while getopts 'n' flag; do
-		case "${flag}" in
-			n) dry_run='true' ;;
-			*) error "Unexpected option ${flag}"; exit 1 ;;
-		esac
-	done
+  while getopts 'n' flag; do
+    case "${flag}" in
+      n) dry_run='true' ;;
+      *) error "Unexpected option ${flag}"; exit 1 ;;
+    esac
+  done
 
-	if [ "$dry_run" = "true" ]; then
-		echo "Dry run"
-	fi
+  if [ "$dry_run" = "true" ]; then
+    echo "Dry run"
+  fi
 
-	for ((i = 0; i < ${#FILES[@]}; ++i)); do
-		file="${FILES[$i]}"
-		source="$FILES_DIR/${FILES[$i]}"
-		destination=${DESTINATIONS[$i]}
-		backup="$BACKUP_DIR/$file"
-		echo "---- $file -----"
-		if [ -L $destination ]; then
-			echo "$destination is already a symlink"
-		else
-			echo "Creating directory $(dirname $backup)"
-			if [ "$dry_run" = "false" ]; then
-				mkdir -p $(dirname $backup)
-			fi
-			if [ -e "$destination" ]; then
-				if [ -e "$backup" ]; then
-				    echo "Backing file $backup already exists"
-				else
-				    echo "Backing up file $destination to $backup"
-					if [ "$dry_run" = "false" ]; then
-						mv "$destination" "$backup"
-					fi
-				fi
-			fi
-		    echo "Creating symlink for file $source in $destination"
-			if [ "$dry_run" = "false" ]; then
-				ln -s "$source" "$destination"
-			fi
-			echo "$file done."
-		fi
-		echo
-	done
+  for ((i = 0; i < ${#FILES[@]}; ++i)); do
+    file="${FILES[$i]}"
+    source="$FILES_DIR/${FILES[$i]}"
+    destination=${DESTINATIONS[$i]}
+    backup="$BACKUP_DIR/$file"
+    echo "---- $file -----"
+    if [ -L $destination ]; then
+      echo "$destination is already a symlink"
+    else
+      echo "Creating directory $(dirname $backup)"
+      if [ "$dry_run" = "false" ]; then
+        mkdir -p $(dirname $backup)
+      fi
+      if [ -e "$destination" ]; then
+        if [ -e "$backup" ]; then
+            echo "Backing file $backup already exists"
+        else
+            echo "Backing up file $destination to $backup"
+          if [ "$dry_run" = "false" ]; then
+            mv "$destination" "$backup"
+          fi
+        fi
+      fi
+        echo "Creating symlink for file $source in $destination"
+      if [ "$dry_run" = "false" ]; then
+        ln -s "$source" "$destination"
+      fi
+      echo "$file done."
+    fi
+    echo
+  done
 }
 
 main "$@"
