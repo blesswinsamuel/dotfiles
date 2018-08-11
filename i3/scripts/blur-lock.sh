c5d05620e33972cb3a28e10f14804724a72ca9f9
Blesswin Samuel <blesswinsamuel@gmail.com>
2018-08-12 04:19:47 +0530

    Update i3 config

---
+++ i3/scripts/blur-lock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR="$( dirname "$0" )"
+
+maim /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png # Pixelate
+# convert /tmp/screen.png -filter Gaussian -blur 0x8 /tmp/screen.png # Blur
+[[ -f $DIR/lock.svg ]] && \
+  convert -density 1200 -background none -resize 186x186 $DIR/lock.svg ~/Pictures/lock.png && \
+  convert /tmp/screen.png ~/Pictures/lock.png -gravity center -composite -matte /tmp/screen.png
+i3lock -i /tmp/screen.png
