05e27b48e1f2a7acfe18448512a1750aaffa89fe
Blesswin Samuel <blesswinsamuel@gmail.com>
2018-09-01 12:32:10 +0530

    Add fish aliases

--- fish/config.fish
+++ fish/config.fish
@@ -19,26 +19,55 @@ alias youtube-dl-mp3 "youtube-dl --extract-audio --audio-format mp3"
 
 alias ix "curl -F 'f:1=<-' ix.io"
 
-switch (uname)
-case Darwin
-	set -gx PATH /usr/local/sbin $PATH
+if type -q bat
+  alias cat bat
+end
+if type -q prettyping
+  alias ping 'prettyping --nolegend'
+end
+if type -q fzf
+  # add support for ctrl+o to open selected file in subl
+  set -x FZF_DEFAULT_OPTS "--bind='ctrl-o:execute(subl {})+abort'"
 
-	# Mac Quick Look
-    alias ql "qlmanage -p"
-	alias dont-index "touch .metadata_never_index"
-case '*'
-    alias pbcopy='xsel --clipboard --input'
-    alias pbpaste='xsel --clipboard --output'
-    # alias pbcopy='xclip -selection clipboard'
-    # alias pbpaste='xclip -selection clipboard -o'
+  # use fd instead of find
+  if type -q fd
+    set -x FZF_DEFAULT_COMMAND 'fd --type f'
+  end
+
+  # preview alias
+  set FZF_PREVIEW_COMMAND 'head -100'
+  if type -q bat
+    set FZF_PREVIEW_COMMAND 'bat --line-range 0:100 --color always'
+  end
+  alias preview "fzf --preview '$FZF_PREVIEW_COMMAND {}'"
+end
+if type -q ncdu
+  alias ncdu 'ncdu --color dark'
 end
 
 switch (uname)
 case Darwin
-    alias o "open"
+  set -gx PATH /usr/local/sbin $PATH
+
+  # Mac Quick Look
+  alias ql "qlmanage -p"
+  alias dont-index "touch .metadata_never_index"
+
+  # cd to open Finder directory
+  alias cdf "cd (osascript -e 'tell application \"Finder\" to get the POSIX path of (target of front window as alias)')"
 case '*'
-    alias open "xdg-open"
-    alias o "xdg-open"
+  alias pbcopy 'xsel --clipboard --input'
+  alias pbpaste 'xsel --clipboard --output'
+  # alias pbcopy 'xclip -selection clipboard'
+  # alias pbpaste 'xclip -selection clipboard -o'
+end
+
+switch (uname)
+case Darwin
+  alias o "open"
+case '*'
+  alias open "xdg-open"
+  alias o "xdg-open"
 end
 
 # My colors
