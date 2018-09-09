9d966851685c1270bcebc69cdc06c2e6cfa60ce7
Blesswin Samuel <blesswinsamuel@gmail.com>
2018-09-10 02:45:10 +0530

    update pacman.sh, wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget -O ~/Pictures/wallpaper.jpg http://www.wallpapers13.com/wp-content/uploads/2016/03/Evening-on-the-blue-Lake-Tekapo-desktop-wallpaper-hd-widescreen-free-download.jpg
+mkdir -p ~/Pictures && wget -O ~/Pictures/wallpaper.jpg http://www.wallpapers13.com/wp-content/uploads/2016/03/Evening-on-the-blue-Lake-Tekapo-desktop-wallpaper-hd-widescreen-free-download.jpg
