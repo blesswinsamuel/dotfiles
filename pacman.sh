5c9b31339d828da83813c3e3dcbb22776e7a3598
Blesswin Samuel <blesswinsamuel@gmail.com>
2018-09-10 23:03:07 +0530

    update packman.sh

--- pacman.sh
+++ pacman.sh
@@ -3,12 +3,13 @@
 USERNAME=${USERNAME:-bless}
 
 sudo pacman -S --needed --noconfirm \
-  git tmux htop wget tree vim reptyr imagemagick networkmanager fish x11vnc ffmpeg maim \
+  git tmux htop wget tree vim reptyr imagemagick networkmanager fish x11vnc ffmpeg maim nfs-utils acpi sysstat \
   xorg-server xorg-xinit xorg-xprop xorg-xbacklight xorg-xinput xsel xclip xautolock i3-gaps i3blocks i3lock rofi compton feh dunst dmenu \
-  pulseaudio alsa-utils pulseaudio-alsa \
+  pulseaudio alsa-utils pulseaudio-alsa pulseaudio-bluetooth \
+  bluez bluez-libs bluez-utils blueman \
   network-manager-applet \
   rxvt-unicode pcmanfm lxappearance simplescreenrecorder flameshot \
-  nodejs python \
+  nodejs python docker docker-compose \
   chromium firefox \
   arc-gtk-theme papirus-icon-theme \
   ttf-dejavu ttf-liberation ttf-inconsolata ttf-fira-code adobe-source-sans-pro-fonts otf-font-awesome \
@@ -25,3 +26,7 @@ sudo ln -sf /usr/bin/subl3 /usr/bin/subl
 
 # Enable services
 systemctl enable NetworkManager.service
+systemctl enable bluetooth.service
+systemctl enable docker.service
+
+usermod -aG docker bless
