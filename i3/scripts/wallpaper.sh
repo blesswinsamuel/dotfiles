49d1604817b70210b9fcfd92733b46f13c768ed6
Blesswin Samuel <blesswinsamuel@gmail.com>
2018-09-11 02:46:16 +0530

    Limit only nature and water wallpapers

--- i3/scripts/wallpaper.sh
+++ i3/scripts/wallpaper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/Pictures && curl -Lo ~/Pictures/wallpaper.jpg "http://source.unsplash.com/1920x1200"
+mkdir -p ~/Pictures && curl -Lo ~/Pictures/wallpaper.jpg "http://source.unsplash.com/1920x1200?nature,water"
 feh --bg-scale $HOME/Pictures/wallpaper.jpg
