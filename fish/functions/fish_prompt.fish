446ed2404c079bf386ccfd032f7896919914c7e9
Blesswin Samuel <blesswinsamuel@gmail.com>
2020-05-01 18:29:19 +0530

    Add comments in fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -14,30 +14,37 @@ function fish_prompt
   set -g __fish_git_prompt_showcolorhints
   set -g __fish_git_prompt_char_stateseparator ' |'
 
+  ## Line 1
   printf "\n"
+
+  # SSH indicator
   if test -n "$SSH_TTY"
     set_color yellow
     printf '⚡ '
     set_color normal
   end
 
+  # Working Directory
   set_color $fish_color_cwd
   printf '%s' (prompt_pwd)
   set_color normal
 
+  # Git Prompt
   if test -n "$fish_prompt_show_git_prompt"
     printf '%s ' (__fish_git_prompt)
     set_color normal
   end
 
+  ## Line 2
   printf "\n"
-
-  set_color yellow
+  # Exit Code
   if not test $last_status -eq 0
     set_color $fish_color_status
     printf "[$last_status]"
     printf " "
   end
+  # Prompt indicator
+  set_color yellow
   printf "❯"
   set_color normal
   printf " "
