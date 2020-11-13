7c632ebe72e3938ed005b3e6e9382ecb94697e25
Blesswin Samuel <blesswinsamuel@gmail.com>
2020-11-13 11:35:26 +0530

    Add git-open alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -15,60 +15,60 @@ alias g "git"
 # alias dlrshell "env PS1='\$ ' bash"
 
 function mkcd
-    mkdir -pv $argv;
-    cd $argv;
+    mkdir -pv $argv
+    cd $argv
 end
 alias cdr "cd (git rev-parse --show-toplevel)"
 
 if type -q bat
-  alias cat bat
+    alias cat bat
 end
 if type -q prettyping
-  alias ping 'prettyping --nolegend'
+    alias ping 'prettyping --nolegend'
 end
 if type -q fzf
-  # add support for ctrl+o to open selected file in subl
-  set -x FZF_DEFAULT_OPTS "--bind='ctrl-o:execute(subl {})+abort'"
+    # add support for ctrl+o to open selected file in subl
+    set -x FZF_DEFAULT_OPTS "--bind='ctrl-o:execute(subl {})+abort'"
 
-  # use fd instead of find
-  if type -q fd
-    set -x FZF_DEFAULT_COMMAND 'fd --type f'
-  end
+    # use fd instead of find
+    if type -q fd
+        set -x FZF_DEFAULT_COMMAND 'fd --type f'
+    end
 
-  # preview alias
-  set FZF_PREVIEW_COMMAND 'head -100'
-  if type -q bat
-    set FZF_PREVIEW_COMMAND 'bat --line-range 0:100 --color always'
-  end
-  alias preview "fzf --preview '$FZF_PREVIEW_COMMAND {}'"
+    # preview alias
+    set FZF_PREVIEW_COMMAND 'head -100'
+    if type -q bat
+        set FZF_PREVIEW_COMMAND 'bat --line-range 0:100 --color always'
+    end
+    alias preview "fzf --preview '$FZF_PREVIEW_COMMAND {}'"
 end
 if type -q ncdu
-  alias ncdu 'ncdu --color dark'
+    alias ncdu 'ncdu --color dark'
 end
 
 switch (uname)
-case Darwin
-  set -gx PATH /usr/local/sbin $PATH
+    case Darwin
+        set -gx PATH /usr/local/sbin ~/.local/bin ~/go/bin ~/bin /usr/local/bin $PATH
 
-  # Mac Quick Look
-  alias ql "qlmanage -p"
-  alias dont-index "touch .metadata_never_index"
+        # Mac Quick Look
+        alias ql "qlmanage -p"
+        alias dont-index "touch .metadata_never_index"
 
-  # cd to open Finder directory
-  alias cdf "cd (osascript -e 'tell application \"Finder\" to get the POSIX path of (target of front window as alias)')"
-case '*'
-  # alias pbcopy 'xsel --clipboard --input'
-  # alias pbpaste 'xsel --clipboard --output'
-  alias pbcopy 'xclip -selection clipboard'
-  alias pbpaste 'xclip -selection clipboard -o'
+        # cd to open Finder directory
+        alias cdf "cd (osascript -e 'tell application \"Finder\" to get the POSIX path of (target of front window as alias)')"
+    case '*'
+        # alias pbcopy 'xsel --clipboard --input'
+        # alias pbpaste 'xsel --clipboard --output'
+        alias pbcopy 'xclip -selection clipboard'
+        alias pbpaste 'xclip -selection clipboard -o'
 end
 
 switch (uname)
-case Darwin
-  alias o "open"
-case '*'
-  alias open "xdg-open"
-  alias o "xdg-open"
+    case Darwin
+        alias o "open"
+    case '*'
+        alias open "xdg-open"
+        alias o "xdg-open"
 end
 
 alias fish-reload "source ~/.config/fish/config.fish"
@@ -83,3 +83,16 @@ end
 
 alias urldecode 'python -c "import sys, urllib as ul; print(ul.unquote_plus(sys.argv[1]))"'
 alias urlencode 'python -c "import sys, urllib as ul; print(ul.quote_plus(sys.argv[1]))"'
+function git-open
+    set -l giturl (git config --get remote.origin.url)
+    if test -z "$giturl"
+        echo "Not a git repository or no remote.origin.url set"
+        return
+    end
+
+    set -l giturl (string replace "git@github.com:" "https://github.com/" $giturl)
+    set -l giturl (string replace ".git" "" $giturl)
+    set -l branch (git branch --show-current)
+    set -l giturl $giturl/tree/$branch
+    open $giturl
+end
