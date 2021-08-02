24540df198e717f50934a45efe30d7b9e7791f0e
Blesswin Samuel <blesswinsamuel@gmail.com>
2021-08-02 07:38:45 +0530

    Change default dotfiles path

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     fi
 fi
 
-DOTFILES_DIR=${DOTFILES_DIR:-~/Projects/blesswinsamuel/dotfiles}
+DOTFILES_DIR=${DOTFILES_DIR:-~/dotfiles}
 
 if [ ! -d "$DOTFILES_DIR" ]; then
     mkdir -p "${DOTFILES_DIR##*/}"
