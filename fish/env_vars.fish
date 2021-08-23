3efbd7decae9442d675e6624282c757751d1a883
Blesswin Samuel <blesswinsamuel@gmail.com>
2021-08-23 06:14:43 +0530

    fix PATH on m1

--- fish/env_vars.fish
+++ fish/env_vars.fish
@@ -1,4 +1,4 @@
-set -gx EDITOR "vim"
+set -gx EDITOR vim
 if test -n "$LANG"
     set -gx LANG en_US.UTF-8
 end
@@ -15,10 +15,10 @@ set -gx HOMEBREW_NO_AUTO_UPDATE 1
 # brew - /usr/local/sbin /usr/local/bin
 # pipx - ~/.local/bin
 # go   - ~/go/bin
-set -gx PATH $PATH /opt/homebrew/bin /usr/local/sbin /usr/local/bin ~/.local/bin ~/go/bin ~/bin
+set -gx PATH /opt/homebrew/bin $PATH /usr/local/sbin /usr/local/bin ~/.local/bin ~/go/bin ~/bin
 
-set PATH $PATH ~/.cargo/bin ~/.config/yarn/global/node_modules/.bin/
+set -gx PATH $PATH ~/.cargo/bin ~/.config/yarn/global/node_modules/.bin/
 
-set PATH $PATH ~/.gem/ruby/2.7.0/bin
+set -gx PATH $PATH ~/.gem/ruby/2.7.0/bin
 
-set PATH $PATH ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/
+set -gx PATH $PATH ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/
