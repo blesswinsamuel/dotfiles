1824bbe00d3b2784094f64eb4b7b3f36c6380a30
Blesswin Samuel <git@bless.win>
2025-11-07 03:26:12 +0530

    fixes

--- hosts/hp-chromebox/hp-chromebox.nix
+++ hosts/hp-chromebox/hp-chromebox.nix
@@ -114,98 +114,97 @@
     isNormalUser = true;
     description = "Blesswin Samuel";
     extraGroups = [ "networkmanager" "wheel" ];
+    packages = [
+      pkgsStable.kdePackages.kate
+
+      # mongodb
+      # mongosh
+      # mongodb-tools
+      pkgsUnstable.redis
+      pkgsUnstable.postgresql_14
+      pkgsUnstable.mailhog
+      # grafana
+      # traefik
+      # victoriametrics
+
+      # Kubernetes
+      pkgsUnstable.kapp
+      pkgsUnstable.kubectl
+      pkgsUnstable.kubernetes-helm
+      # pkgs.kopia
+      pkgsUnstable.krew
+      pkgsUnstable.stern
+      pkgsUnstable.kubie
+
+      # Docker
+      pkgsUnstable.podman
+      pkgsUnstable.podman-compose
+      pkgsUnstable.podman-tui
+
+      # Infrastructure management
+      # pkgsMaster.terraform
+      # nodePackages.cdk8s-cli
+      # pkgsMaster.nodePackages.cdktf-cli
+
+      # TUI
+      pkgsUnstable.gitui
+
+      # pkgs.yt-dlp
+      # pkgs.ffmpeg
+      # pkgs.imagemagick
+
+      # Tools
+      pkgsUnstable.autossh
+      pkgsUnstable.direnv
+      pkgsUnstable.atuin
+      pkgsUnstable.just
+      pkgsUnstable.neovim
+      pkgsUnstable.gojq
+      pkgsUnstable.hey # HTTP load generator, ApacheBench (ab) replacement
+      pkgsUnstable.rclone
+      pkgsUnstable.qpdf
+      pkgsUnstable.pv
+      pkgsUnstable.gnused
+      # pkgs.gnutar
+      # pkgs.gzip
+      # pkgs.unzip
+      pkgsUnstable.websocat
+      # pkgsMaster.unison
+      pkgsUnstable.hexyl # command line hex viewer
+      # qrcp # transfer files over Wi-Fi from your computer to a mobile device by scanning a QR code without leaving the terminal
+      pkgsUnstable.smartmontools # tools for monitoring the health of hard drives
+      pkgsUnstable.syncthing # continuous file synchronization program
+      pkgsUnstable.wakeonlan # sends magic packets to wake up network-devices
+      pkgsUnstable.mkcert # simple tool for making locally-trusted development certificates
+      pkgsUnstable.delta # syntax-highlighting pager for git
+
+      # 3rd party cloud service tools
+      # pkgsMaster.awscli2
+      # pkgs.bitwarden-cli
+      pkgsUnstable.gh
+
+      pkgsUnstable.teller
+      pkgsUnstable.git-secrets
+      pkgsUnstable.gitleaks
+
+      # pkgs.home-assistant-cli
+
+      pkgsUnstable.xclip
+
+      # GUIs
+      pkgsUnstable.google-chrome
+      pkgsUnstable.vscode
+      pkgsUnstable.telegram-desktop
+      pkgsUnstable.slack
+      pkgsUnstable.sublime-merge
+      pkgsUnstable.sublime4
+      pkgsUnstable.github-desktop
+      pkgsUnstable.zed-editor
+      pkgsUnstable.obsidian
+      pkgsUnstable.ghostty
+      pkgsUnstable.kdePackages.krdc
+    ];
   };
-
-  users.users.${systemConfig.username}.packages = [
-    pkgsStable.kdePackages.kate
-
-    # mongodb
-    # mongosh
-    # mongodb-tools
-    pkgsUnstable.redis
-    pkgsUnstable.postgresql_14
-    pkgsUnstable.mailhog
-    # grafana
-    # traefik
-    # victoriametrics
-
-    # Kubernetes
-    pkgsUnstable.kapp
-    pkgsUnstable.kubectl
-    pkgsUnstable.kubernetes-helm
-    # pkgs.kopia
-    pkgsUnstable.krew
-    pkgsUnstable.stern
-    pkgsUnstable.kubie
-
-    # Docker
-    pkgsUnstable.podman
-    pkgsUnstable.podman-compose
-    pkgsUnstable.podman-tui
-
-    # Infrastructure management
-    # pkgsMaster.terraform
-    # nodePackages.cdk8s-cli
-    # pkgsMaster.nodePackages.cdktf-cli
-
-    # TUI
-    pkgsUnstable.gitui
-
-    # pkgs.yt-dlp
-    # pkgs.ffmpeg
-    # pkgs.imagemagick
-
-    # Tools
-    pkgsUnstable.autossh
-    pkgsUnstable.direnv
-    pkgsUnstable.atuin
-    pkgsUnstable.just
-    pkgsUnstable.neovim
-    pkgsUnstable.gojq
-    pkgsUnstable.hey # HTTP load generator, ApacheBench (ab) replacement
-    pkgsUnstable.rclone
-    pkgsUnstable.qpdf
-    pkgsUnstable.pv
-    pkgsUnstable.gnused
-    # pkgs.gnutar
-    # pkgs.gzip
-    # pkgs.unzip
-    pkgsUnstable.websocat
-    # pkgsMaster.unison
-    pkgsUnstable.hexyl # command line hex viewer
-    # qrcp # transfer files over Wi-Fi from your computer to a mobile device by scanning a QR code without leaving the terminal
-    pkgsUnstable.smartmontools # tools for monitoring the health of hard drives
-    pkgsUnstable.syncthing # continuous file synchronization program
-    pkgsUnstable.wakeonlan # sends magic packets to wake up network-devices
-    pkgsUnstable.mkcert # simple tool for making locally-trusted development certificates
-    pkgsUnstable.delta # syntax-highlighting pager for git
-
-    # 3rd party cloud service tools
-    # pkgsMaster.awscli2
-    # pkgs.bitwarden-cli
-    pkgsUnstable.gh
-
-    pkgsUnstable.teller
-    pkgsUnstable.git-secrets
-    pkgsUnstable.gitleaks
-
-    # pkgs.home-assistant-cli
-
-    pkgsUnstable.xclip
-
-    # GUIs
-    pkgsUnstable.google-chrome
-    pkgsUnstable.vscode
-    pkgsUnstable.telegram-desktop
-    pkgsUnstable.slack
-    pkgsUnstable.sublime-merge
-    pkgsUnstable.sublime4
-    pkgsUnstable.github-desktop
-    pkgsUnstable.zed-editor
-    pkgsUnstable.obsidian
-    pkgsUnstable.ghostty
-    pkgsUnstable.kdePackages.krdc
-  ];
   programs.firefox.enable = true;
   programs.firefox.package = pkgsUnstable.firefox;
   nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
